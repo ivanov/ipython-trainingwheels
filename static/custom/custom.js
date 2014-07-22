a15ce42c87641bae6db33d568617e5b7c8158fab
Paul Ivanov <pi@berkeley.edu>
2014-07-22 15:00:35 -0700

    added a "standard imports" button
    also added an "external link" indicator for the training wheels website
    link itself.

--- static/custom/custom.js
+++ static/custom/custom.js
@@ -1,3 +1,21 @@
+
+standar_imports = [
+		'import os',
+        'import sys',
+        'import numpy as np',
+        'import matplotlib.pyplot as plt',
+		'# these imports can be changed in IPython Training Wheels',
+        ].join("\n")
+
+function std_imports() {
+    var cell = IPython.notebook.insert_cell_at_index('code', 0);
+    cell.set_text(standar_imports);
+    IPython.notebook.select(0);
+
+    console.log("standard imports");
+
+}
+
 $([IPython.events]).on('notebook_loaded.Notebook', function(){
 
     $([
@@ -20,9 +38,13 @@ $([IPython.events]).on('notebook_loaded.Notebook', function(){
 	$([
 		'<li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Training Wheels</a>',
         '<ul id="training_wheels" class="dropdown-menu">',
-        '<li id="training_wheels"',
-        'title="IPython Training Wheels">',
-        '<a href="http://github.com/ivanov/ipython-trainingwheels">IPython Training Wheels</a></li>',
+        '<li id="std-imports" title="Insert a new cell with imports of commonly used modules">',
+        '<a href="#" onclick="std_imports();">Standard imports </a></li>',
+        '<li class="divider"></li>',
+        '<li id="training_wheels" title="IPython Training Wheels">',
+        '<a href="http://github.com/ivanov/ipython-trainingwheels" target="_blank" title="Training Wheels Website">',
+        '<i class="icon-external-link menu-icon pull-right"></i>',
+        ' IPython Traning Wheels &nbsp;&nbsp;&nbsp;&nbsp;       </a> </li>',
 		'</ul>',
 		'</li>',
         ].join("\n")).insertAfter($('#help_menu').parent());
